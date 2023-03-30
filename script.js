057ef1fef10d987bf29e6c6444169dd18d4115ef
Chinenye <83243717+Birdie13-stack@users.noreply.github.com>
2023-03-30 12:49:38 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const effect = document.getElementById("image1");
+
+effect.addEventListener('click', function(e) {
+  e.target.style.color = "blue";
+  e.target.style.border = "2px solid skyblue";
+  e.target.textContent = "I'm sure I'm not the only one who loves this";
+})
+
+const effect2 = document.getElementById("image2");
+
+effect2.addEventListener("click", function (e) {
+  e.target.style.color = "blue";
+  e.target.style.border = "2px solid skyblue";
+  e.target.textContent = "A taste of local food never hurts";
+});
+
+const effect3 = document.getElementById("image3");
+
+effect3.addEventListener("click", function (e) {
+  e.target.style.color = "blue";
+  e.target.style.border = "2px solid skyblue";
+  e.target.textContent = "Take me to the seventh heavens of delight";
+});
+
+const effect4 = document.getElementById("image4");
+
+effect4.addEventListener("click", function (e) {
+  e.target.style.color = "blue";
+  e.target.style.border = "2px solid skyblue";
+  e.target.textContent = "I don't think anyone on this planet loves meatpies more than I do 🥲";
+});
+
+const effect5 = document.getElementById("image5");
+
+effect5.addEventListener("click", function (e) {
+  e.target.style.color = "blue";
+  e.target.style.border = "2px solid skyblue";
+  e.target.textContent = "With bread and hot tea, this can never go wrong";
+});
+
+const effect6 = document.getElementById("image6");
+
+effect6.addEventListener("click", function (e) {
+  e.target.style.color = "blue";
+  e.target.style.border = "2px solid skyblue";
+  e.target.textContent = "No matter what you do, don't ever miss the egg 😜";
+});
+
+const effect7 = document.getElementById("image7");
+
+effect7.addEventListener("click", function (e) {
+  e.target.style.color = "blue";
+  e.target.style.border = "2px solid skyblue";
+  e.target.textContent = "If there's shredded chicken in the toast, I'm sold";
+});
+
+const effect8 = document.getElementById("image8");
+
+effect8.addEventListener("click", function (e) {
+  e.target.style.color = "blue";
+  e.target.style.border = "2px solid skyblue";
+  e.target.textContent = "Once it's anything yam, count me in";
+});
